d7065eeeb2ee20dc3ad0b83a98338f55488ddaab
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-04 18:50:26 -0500

    Proyecto iniciado con validación de cuenta para el blog

---
+++ class/Login/Login.class.php
@@ -0,0 +1,31 @@
+<?php
+class Login
+{
+    private $con;
+    public $email;
+    public $password; 
+
+    //Inyeccion de Dependencias
+    public function __construct(Conexion $con)
+    {
+        $this->con = $con;
+    }
+    public function setEmail(string $email){
+        $this->email = $email;
+    }
+    public function setPassword(string $password){
+        $this->password = $password;
+    }
+    public function signIn(){
+        $query = "SELECT * FROM `usuario` WHERE email_dev = '$this->email' AND password_dev = '$this->password' ";
+        $res = $this->con->query($query);
+        if($this->con->affected_rows > 0 )
+        {
+            return $res->fetch_array(MYSQLI_ASSOC);
+        }else{
+            return false;
+        }
+
+    }
+}
+?>
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('CONF_DB_HOST', 'localhost');
+define ('CONF_DB_USER','root');
+define('CONF_DB_PASS','root');
+define('CONF_DB_DATABASE','bd_josuemart');
+define('CONF_DB_CHARSET','utf8');
+?>
