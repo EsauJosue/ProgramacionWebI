4d334508a6a15a9b166ea935d387f83922e27c60
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-05 15:48:10 -0500

    Cambios en los menus del navbar

--- dashboard.php
+++ dashboard.php
@@ -58,14 +58,14 @@ if(! $session->validateSession('id'))
   </head>
   <body>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Josué Martínez</a>
+  <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#"><?php echo $session->getValue('email') ?></a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
   <ul class="navbar-nav px-3">
-    <li class="nav-item text-nowrap">
-      <a class="nav-link" href="#">Sign out</a>
+     <li class="nav-item text-nowrap">
+      <a class="nav-link" href="#">Cerrar Sesion</a>
     </li>
   </ul>
 </nav>
@@ -78,13 +78,13 @@ if(! $session->validateSession('id'))
           <li class="nav-item">
             <a class="nav-link active" href="#">
               <span data-feather="home"></span>
-              Dashboard <span class="sr-only">(current)</span>
+              Ir al Blog <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">
               <span data-feather="file"></span>
-              Orders
+              Cambiar contraseña
             </a>
           </li>
           <li class="nav-item">
@@ -96,7 +96,7 @@ if(! $session->validateSession('id'))
           <li class="nav-item">
             <a class="nav-link" href="#">
               <span data-feather="users"></span>
-              Customers
+              Cerrar sesion
             </a>
           </li>
           <li class="nav-item">
