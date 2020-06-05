4be2c959a618c88ef5c15df114a8d4b558f4f3cd
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-04 20:46:53 -0500

    Refactorizacion de la funcion signin, dividiendola en tres partes

--- class/Login/Login.class.php
+++ class/Login/Login.class.php
@@ -18,20 +18,33 @@ class Login
         $this->password = $password;
     }
     public function signIn(){
-        $query = "SELECT * FROM `usuario` WHERE email_dev = '$this->email'";
-        echo $query;
-        $res = $this->con->query($query);
-        
-        if($this->con->affected_rows > 0 )
+        $row = $this->getArrayQueryResult();
+        if($this->isAffectedRows())
         {
-            $row = $res->fetch_array(MYSQLI_ASSOC);
-            if(password_verify($this->password, $row['password_dev'])){
+            if($this->passwordVerify($row['password_dev'])){
                  return $row;
             }
         }else{
             return false;
         }
 
+
     }
+
+    public function getArrayQueryResult(){
+        $query = "SELECT * FROM `usuario` WHERE email_dev = '$this->email'";
+        $result = $this->con->query($query);
+        return $result->fetch_array(MYSQLI_ASSOC);
+    }
+    public function isAffectedRows () : bool{
+        return ($this->con->affected_rows > 0);
+    }
+
+    public function passwordVerify ($password) : bool {
+
+        return password_verify($this->password, $password);
+
+    }
+    
 }
 ?>
